f5c008cb4bae80facb10fc5ca2db764e056c1f3e
Stella-coder <sanistella7@gmail.com>
2021-10-15 16:01:47 +0100

    Push

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 4000;
 const url = process.env.MONGODB_URL;
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose
   .connect(url, { useNewUrlParser: true })
